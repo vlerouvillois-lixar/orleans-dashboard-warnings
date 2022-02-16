0867be3b054fc0d77af4e5f9620f6910d3903d5c
vincent.lerouvillois <vlerouvillois@lixar.com>
2022-02-16 16:25:18 -0500

    First commit

---
+++ OrleansTest2.Grains/FlightGrain.cs
@@ -0,0 +1,9 @@
+﻿using Orleans;
+using System;
+
+namespace OrleansTest2.Grains
+{
+    public class FlightGrain : Grain, IGrainWithIntegerKey
+    {
+    }
+}
---
+++ OrleansTest2/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Orleans;
+using Orleans.Hosting;
+using System.Threading.Tasks;
+using OrleansTest2.Grains;
+
+namespace OrleansTest2
+{
+    class Program
+    {        
+        static async Task Main(string[] args)
+        {
+            await Host.CreateDefaultBuilder(args)
+                .UseOrleans((ctx, builder) =>
+                {
+                    builder
+                    .UseLocalhostClustering()
+                    .UseDashboard(options =>
+                    {
+                        options.Port = 8888;
+                    })
+                    .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(FlightGrain).Assembly));
+                })
+                .RunConsoleAsync();
+        }
+    }
+}
